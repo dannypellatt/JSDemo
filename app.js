085bf242040686f79b9e6f9fdf88c937a68a7df8
dannypellatt <danielpellatt92@gmail.com>
2022-08-08 12:35:12 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const firstName = undefined;
+let lastName = undefined;
+var age = 30;
+
+firstName = "Danny";
+lastName = "Pellatt";
+age = 30;
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
