3fd16c63a60eaefc5186fb51be934ad43c4643e9
dannypellatt <danielpellatt92@gmail.com>
2022-08-08 13:50:01 -0400

    first 3 exercises complete

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const firstName = undefined;
 let lastName = undefined;
 var age = 30;
+let ex1 = document.getElementById("ex1");
+ex1.innerHTML = "I am " + age + " years old";
 
 firstName = "Danny";
 lastName = "Pellatt";
 age = 30;
+let ex2 = document.getElementById("ex2");
+ex2.innerHTML = `My name is ${firstName} ${lastName} and I am ${age} years old`; 
 
 let language = "JavaScript";
 let createdYear = 1995;
 let isCaseSensitive = true;
+let ex3 = document.getElementById("ex3");
+ex3.innerHTML = "The language " + language + " was created in " + createdYear + ". It is case sensitive." ;
+
 
 let price = 19.99;
 let isOnSale = false;
